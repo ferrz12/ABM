050492a9ba6e6c54bb489b46a085bb618485c3c1
alumno <alumno>
2018-09-24 15:20:06 -0300

    completar

--- abmAlumnos/main.c
+++ abmAlumnos/main.c
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 #include "funcionesAlumnos.h"
 #include "funcionesExtra.h"
-#define T 3
+#define T 10
+
+void inicializarAlumnos(eAlumno [], int);
 
 int main()
 {
@@ -20,7 +22,7 @@ int main()
         {
         case 1:
 
-
+            /*
             if(cargarAlumnosPorEspacioLibre(listado, T)!=-1)
             {
                 printf("Carga satisfactoria");
@@ -29,6 +31,8 @@ int main()
             {
                 printf("No hay espacio!");
             }
+            */
+            inicializarAlumnos(listado, T);
             break;
         case 2:
             mostrarListadoDeAlumnos(listado, T);
@@ -75,3 +79,26 @@ int main()
 
     return 0;
 }
+
+
+void inicializarAlumnos(eAlumno listado[], int tam){
+
+    //inicializa solo 3
+
+    int legajo[3] = {5, 9, 7};
+    char nombre[3][50] = {"Juan", "Maria", "Luis"};
+    float altura[3] = {1.65, 1.70, 1.80};
+    int nota[3] = {5, 8, 9};
+
+    int i;
+    for(i = 0; i < 3; i++)
+    {
+        listado[i].legajo = legajo[i];
+        listado[i].altura = altura[i];
+        listado[i].nota = nota[i];
+        strcpy(listado[i].nombre, nombre[i]);
+        listado[i].estado = OCUPADO;
+    }
+
+}
+
